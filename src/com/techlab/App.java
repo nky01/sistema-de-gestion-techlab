1f5377093a977f14c53b24aab9c2bddee54ede8e
Nicole Cayo <nickybelu@gmail.com>
2026-09-19 23:43:42 -0300

    implement Add and List products functions and leerEntero and Double

--- src/com/techlab/App.java
+++ src/com/techlab/App.java
@@ -1,12 +1,19 @@
 package com.techlab;
 
+import java.beans.PropertyDescriptor;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Menu {
+import com.techlab.Productos.Producto;
 
-    public static void main(){
+public class App {
+
+    private static ArrayList<Producto> productos = new ArrayList<>();
+
+    public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
+        int opcion;
 
         do{
             System.out.println("===================================");
@@ -24,16 +31,66 @@ public class Menu {
             opcion= leerEntero(scanner, "Ingrese una opcion: ");
 
             switch (opcion) {
-                case value:
-                    
+                case 1 : 
+                    agregarProducto(scanner);
                     break;
-            
-                default:
+                case 2 : 
+                    listarProductos();
+                    break;
+                default: 
+                    System.out.println("Opcion incorrecta. Intente de nuevo...");
                     break;
             }
         }
         while(opcion!=7);
 
+        scanner.close();
+    }
+
+    public static void agregarProducto(Scanner sc){
+
+        Producto producto = new Producto();
+
+        System.out.println("===================================");
+        System.out.println("AGREGAR PRODUCTO");
+        System.out.println("===================================");
+
+        long id = leerEntero(sc, "ID del producto: ");
+        producto.setId(id);
+
+        System.out.println("Nombre del producto: ");
+        String nombre = sc.nextLine();
+
+        while (nombre.trim().isEmpty()) {
+        nombre = sc.nextLine();
+    }
+    producto.setNombre(nombre);
+
+        double precio = leerDouble(sc, "Precio: ");
+        producto.setPrecio(precio);
+        int stock = leerEntero(sc, "Stock: ");
+        producto.setStock(stock);
+
+        productos.add(producto);
+        System.out.println("Producto agregado con exito!");
+    }
+
+    public static ArrayList<Producto> listarProductos(){
+
+        System.out.println("===================================");
+        System.out.println("LISTAR PRODUCTOS");
+        System.out.println("===================================");
+
+        if (productos.isEmpty()) {
+            System.out.println("No hay productos registrados.");
+            return productos;
+        }
+
+        for (int i = 0; i < productos.size(); i++) {
+            System.out.println((i+1) + ". " + productos.get(i));
+        }
+
+        return productos;
     }
 
     public static int leerEntero(Scanner scanner, String mensaje){
@@ -43,6 +100,17 @@ public class Menu {
                 return Integer.parseInt(scanner.nextLine());
             } catch (NumberFormatException e) {
                 System.out.println("Error: Debe ingresar un numero entero valido.");
+            }
+        }
+    }
+
+    public static double leerDouble(Scanner scanner, String mensaje) {
+        while (true) {
+            try {
+                System.out.print(mensaje);
+                return Double.parseDouble(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Debe ingresar un numero decimal valido.");
             }
         }
     }
--- src/com/techlab/Pedidos/Pedido.java
+++ src/com/techlab/Pedidos/Pedido.java
@@ -1,5 +1,15 @@
 package com.techlab.Pedidos;
 
+import java.util.ArrayList;
+
+import com.techlab.Productos.Producto;
+
 public class Pedido {
-  
+    private Long id;
+    private ArrayList<Producto> productos;
+    
+    public static double costoTotal(double precio, int cantidad){
+        double total = precio * cantidad;
+        return total;
+    }
 }
